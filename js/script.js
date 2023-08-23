83390de879d192c04ee3f69bfa72dca1e33bbe7d
maks safonov <safonovmaks@MacBook-Pro-maks.local>
2023-08-23 06:40:24 +0300

    add select api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
-const citySelector = document.querySelector('#city')
-const cityChoices = new Choices(citySelector, {
-	searchEnabled: false,
-	itemSelectText: '',
-})
+const API_URL = 'https://workspace-methed.vercel.app/'
+const LOCATION_URL = 'api/locations'
+
+const getData = async (url, cbSuccess, cbError) => {
+	try {
+		const responce = await fetch(url)
+		const data = await responce.json()
+		cbSuccess(data)
+	} catch (err) {
+		cbError(err)
+	}
+}
+
+const init = () => {
+	const citySelector = document.querySelector('#city')
+	const cityChoices = new Choices(citySelector, {
+		itemSelectText: '',
+	})
+
+	getData(
+		`${API_URL}${LOCATION_URL}`,
+		locationData => {
+			const locations = locationData.map(location => ({
+				value: location,
+			}))
+			cityChoices.setChoices(locations, 'value', 'label', true)
+		},
+		err => {
+			console.log(err)
+		}
+	)
+}
+
+init()
